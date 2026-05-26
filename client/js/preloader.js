cb40a74dd4d06a17a1a97bfc7526acdb22588181
henryBCara <pdrhwolf@gmail.com>
2026-05-26 13:09:47 +0000

    scene2 atualizada melhorada mais nova pasta de naves com sprites com letras minsculas

--- client/js/preloader.js
+++ client/js/preloader.js
@@ -59,12 +59,8 @@ class preloader extends Phaser.Scene {
       frameWidth: 322,
       frameHeight: 51,
     });
-
-    this.load.spritesheet("nave", "naveet.png", {
-      frameWidth: 322,
-      frameHeight: 51,
-    });
-
+    
+    
     this.load.tilemapTiledJSON("todasfases", "mapasv4/todasfases.json");
 
     this.load.image("remasterized", "assets-usados/remasterized.png");
@@ -197,6 +193,17 @@ class preloader extends Phaser.Scene {
     this.load.font("sarpanchregular", "sarpanchregular.otf");
 
     this.load.font("sarpanchextrabold", "sarpanchextrabold.otf");
+
+    // Exemplo de como você vai ter de carregar os tiros no futuro:
+    this.load.image("ativaraliens", "ativaraliens.png")
+    this.load.image("nave-1", "naves/nave-1.png");
+    this.load.image("nave-2", "naves/nave-2.png");
+    this.load.image("nave-3", "naves/nave-3.png");
+    this.load.image("nave-4", "naves/nave-4.png");
+    this.load.image("nave-5", "naves/nave-5.png");
+    this.load.image("naveet", "naves/naveet.png");
+    // this.load.image("tiro_player1", "naves/tiro1.png");
+    // this.load.image("tiro_inimigo", "naves/tiro_et.png");
   }
 
   create() {
