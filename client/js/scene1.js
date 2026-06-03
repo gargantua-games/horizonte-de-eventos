11c11ddeb61107b9271829df68d74c603c9902fb
Luiza Schell Bertol <luiza.sb01@aluno.ifsc.edu.br>
2026-06-03 14:07:22 +0000

    tutorial quebra-cabeça

--- client/js/scene1.js
+++ client/js/scene1.js
@@ -822,7 +822,7 @@ class scene1 extends Phaser.Scene {
         if (this.playerroxo.y > 415) {
           if (!this.puzzleAberto) {
             this.puzzleAberto = true;
-            this.scene.launch("helldivers", {
+            this.scene.launch("quebraCabeca", {
               portaId: 1,
               cenaOrigem: "scene1",
             });
