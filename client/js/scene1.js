6194843961a3239ea015ea4966cd2930ad26fb80
Luiza Schell Bertol <luiza.sb01@aluno.ifsc.edu.br>
2026-06-03 14:31:08 +0000

    começar tutorial tetravex

--- client/js/scene1.js
+++ client/js/scene1.js
@@ -822,7 +822,7 @@ class scene1 extends Phaser.Scene {
         if (this.playerroxo.y > 415) {
           if (!this.puzzleAberto) {
             this.puzzleAberto = true;
-            this.scene.launch("termo", {
+            this.scene.launch("tetravex", {
               portaId: 1,
               cenaOrigem: "scene1",
             });
