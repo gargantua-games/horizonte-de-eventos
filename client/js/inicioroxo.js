80d2a1e2c32557f2242cb026df01a63c1d2b98a4
rafaelns17 <rafael.ns17@aluno.ifsc.edu.br>
2026-06-12 07:06:31 -0300

    Atualizar o inicioroxo.js

--- client/js/inicioroxo.js
+++ client/js/inicioroxo.js
@@ -31,6 +31,11 @@ class inicioroxo extends Phaser.Scene {
     //this.terminal = this.add.image(200, 0, "terminal").setOrigin(0, 0);
     bg.setAlpha(0);
 
+    this.manche = this.add.image(150, 0, "funcaomanche").setOrigin(0, 0).setDepth(999);
+    setTimeout(() => {
+      this.manche.setVisible(false);
+    }, 8000);
+
 
     this.text1 = this.add.text(
       322,
